5743e2ce635ab88e9b364be764fdcd56d739c1f7
tedu <tedu@DESKTOP-67KFJT0>
2019-05-06 09:29:07 +0800

    hyguythg

--- test01/src/main/java/test01/test.java
+++ test01/src/main/java/test01/test.java
@@ -4,5 +4,7 @@ public class test {
 		public static void main(String[] args) {
 			System.out.println("传上来了.......................");
 			System.out.println("dsfdsfsdf");
+			System.out.println("dsfdsfsdf");
+			System.out.println("dsfdsfsdf");
 		}
 }
